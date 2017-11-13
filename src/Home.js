7b5ed863bb3609154e187d0ea786b5ae770cf484
melanie <hello@berkley.io>
2017-11-12 19:57:24 -0800

    add links to about and blog

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,11 @@ class Home extends Component {
     return (
       <div>
         <p>
-          Hi, I'm <a href="">Melanie Berkley</a>.
+          Hi, I'm <a href="/#/about">Melanie Berkley</a>.
         </p>
 
         <p> 
-          <a href="">This is my blog</a>. You can reach me on <a href="https://twitter.com/eislz">Twitter</a>, 
+          <a href="/#/posts">This is my blog</a>. You can reach me on <a href="https://twitter.com/eislz">Twitter</a>, 
           or send me an <a href="mailto:hello@berkley.io">Email</a>. 
           There's also <a href="http://www.linkedin.com/in/melanieberkley">LinkedIn</a> and 
           my projects on <a href="http://www.github.com/her">GitHub</a>. 
